4923f5620e199ac66e3122de9eefeb624f5aac00
LHY-255 <806437959@qq.com>
2026-03-06 07:17:38 +0800

    fix clusterfuzzlite linker error by using lld

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash -eu
 
+# 强制 Clang 使用 lld 链接器，防止 ld.gold 在处理 ASan 调试信息时崩溃
+export CFLAGS="$CFLAGS -fuse-ld=lld"
+export CXXFLAGS="$CXXFLAGS -fuse-ld=lld"
+export LDFLAGS="$LDFLAGS -fuse-ld=lld"
+
 PX4_FUZZ=1 make px4_sitl
 cp build/px4_sitl_default/bin/px4 $OUT/px4
